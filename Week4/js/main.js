443e5779e24ac6420515206b1326fcc55b8775dc
AshishKumarBhagwani <ashishbhagwani26@gmail.com>
2017-06-04 23:18:48 +0500

    commit

---
+++ Week4/js/main.js
@@ -0,0 +1,39 @@
+var DATA = [];
+  var inputText;
+
+function addTodo(){
+  inputText = document.getElementById('input');
+  //check if the text field is empty
+  if(inputText.value!==""){
+      // Check browser support
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        DATA.push(inputText.value);
+        localStorage.setItem("allEntries", JSON.stringify(DATA));
+        inputText.value = '';
+      }
+      else {
+        document.getElementById("container").innerHTML = "Sorry, your browser does not support Web Storage...";
+      }
+    }
+    else {
+      alert('Please mention what to add in list!!');
+    }
+  }
+
+
+
+function load(){
+  // Retrieve
+  document.getElementById("container").innerHTML =" ";
+  var obj=JSON.parse(localStorage.getItem("allEntries"));
+  for (var i = 0; i < obj.length; i++) {
+      document.getElementById("container").innerHTML += "<input type='checkbox'/>"+obj[i]+"<br>";
+  }
+
+}
+function remove(){
+  alert('List will be deleted!!!');
+  localStorage.removeItem("allEntries");
+  document.getElementById("container").innerHTML =" ";
+}
